ff1b6412838707f5b44249f7831f8997d24a00c1
ayusclg <freelancerayus@gmail.com>
2025-03-28 12:35:24 +0545

    Cloudinary Setup

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as Cloud } from "cloudinary";
+import fs from 'fs'
+
+    // Configuration
+    Cloud.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });
+    
+
+const uploadCloud = async(localFilePath)=>
+{
+    try {
+        if(!localFilePath) return;
+        const response = Cloud.uploader.upload(localFilePath,{
+            folder:"E-LEARN", resource_type:"auto"
+        })
+        fs.unlinkSync(localFilePath)
+        return response
+        
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        console.log(error,"error in cloudinary")
+    }
+}
+export {uploadCloud}
