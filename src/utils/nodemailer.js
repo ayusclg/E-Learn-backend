018722ba4a016313e0c1a2744de6686089597717
ayusclg <freelancerayus@gmail.com>
2025-03-29 14:48:15 +0545

    Nodemailer

---
+++ src/utils/nodemailer.js
@@ -0,0 +1,29 @@
+import nodemailer from 'nodemailer'
+
+
+const transport = nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:"",
+        pass:""
+    }
+})
+
+const sendMail = async function (to,subject,text,html) {
+    try {
+        const mailOptions = {
+            from:user,
+            to,
+            subject,
+            text,
+            html,
+    
+        }
+        const responsE = await transport.sendMail(mailOptions)
+        console.log(responsE.response)
+    } catch (error) {
+        console.log("error in sending mail",error)
+    }
+    
+}
+export {sendMail}
