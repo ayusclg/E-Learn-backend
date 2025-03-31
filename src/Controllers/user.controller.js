049c1cd099d7fd5df32f76941ddb3bb35d26c456
ayusclg <freelancerayus@gmail.com>
2025-03-31 10:21:40 +0545

    UserLogin

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -1,82 +1,126 @@
-import { Student } from "../Models/student"
-import { User } from "../Models/user"
-import { uploadCloud } from "../utils/Cloudinary"
+import { Student } from "../Models/student";
 
+import { uploadCloud } from "../utils/Cloudinary";
 
+const refreshTokenGenerate = async (StudentId) => {
+  try {
+    const student = await Student.findById(StudentId);
+    const refreshToken = await student.genrateRefreshToken();
+    student.refresh_token = refreshToken;
+    await student.save({
+      validateBeforeSave: false,
+    });
+    return refreshToken;
+  } catch (error) {
+    console.log("error in generating refreshToken", error);
+  }
+};
 
-const userReg = async(req,res)=>{
-    try {
-        const {firstName,lastName,address,email,password,school,phone} = req.body
+const acessTokenGenerate = async (StudentId) => {
+  try {
+    const student = await Student.findById(StudentId);
+    const acessToken = await student.generateAcessToken();
+    return acessToken;
+  } catch (error) {
+    console.log("error in generating acessToken", error);
+  }
+};
 
-        const userCheck = await User.findOne({email:email})
-        if(userCheck){
-            return res.status(404).json({
-                message:"User Already Registered"
-            })
-        }
-        const photo = `req.file/public/Student/${req.file.filename} `
-        if(!photo){
-            return res.status(404).json({
-                message:"Photo not Found"
-            })
-        }
-        
-        const inCloud = await uploadCloud(photo)
-        const createStudent = await Student.create({
-            firstName,
-            lastName,
-            phone,
-            address,
-            school,
-            email,
-            password,
-            studentPhoto:inCloud.secure_url,
-        })
+const userReg = async (req, res) => {
+  try {
+    const { firstName, lastName, address, email, password, school, phone } =
+      req.body;
 
-        const createdStudent = await Student.findById(createStudent._id).select("-password -refresh_token")
-        if(!createdStudent){
-            return res.status(401).json({
-                message:"Student Not Created"
-            })
-        }
-
-        res.status(201).json({
-            message:"Student Sucessfully Created",
-            data:createdStudent
-        })
-    } catch (error) {
-        res.status(500).json({
-            message:"Server Error In UserReg",
-            error:error
-        })
+    const userCheck = await User.findOne({ email: email });
+    if (userCheck) {
+      return res.status(404).json({
+        message: "User Already Registered",
+      });
     }
-}
-
-
-const userLogin = async(req,res)=>{
-    try {
-        const {email,password} = req.body
-
-        const user = await Student.findOne({email:email})
-        if(!user){
-            return res.status(403).json({
-                message:"User Not Found"
-            })
-        }
-        const passwordCheck = await isPasswordCorrect(password)
-        if(!passwordCheck){
-            return res.status(400).json({
-                message:"Invalid Password"
-            })
-        }
-        
-    } catch (error) {
-        res.status(500).json({
-            message:"Server Error In UserLogin"
-        })
+    const photo = `req.file/public/Student/${req.file.filename} `;
+    if (!photo) {
+      return res.status(404).json({
+        message: "Photo not Found",
+      });
     }
-}
 
+    const inCloud = await uploadCloud(photo);
+    const createStudent = await Student.create({
+      firstName,
+      lastName,
+      phone,
+      address,
+      school,
+      email,
+      password,
+      studentPhoto: inCloud.secure_url,
+    });
 
+    const createdStudent = await Student.findById(createStudent._id).select(
+      "-password -refresh_token"
+    );
+    if (!createdStudent) {
+      return res.status(401).json({
+        message: "Student Not Created",
+      });
+    }
 
-export {userReg}
+    res.status(201).json({
+      message: "Student Sucessfully Created",
+      data: createdStudent,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error In UserReg",
+      error: error,
+    });
+  }
+};
+
+const userLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await Student.findOne({ email: email });
+    if (!user) {
+      return res.status(403).json({
+        message: "User Not Found",
+      });
+    }
+    const passwordCheck = await isPasswordCorrect(password);
+    if (!passwordCheck) {
+      return res.status(400).json({
+        message: "Invalid Password",
+      });
+    }
+
+    const acesstoken = await acessTokenGenerate(user._id);
+    const refreshtoken = await refreshTokenGenerate(user._id);
+    if (!acesstoken || !refreshtoken) {
+      return res.status(404).json({
+        message: "Token Not Found",
+      });
+    }
+    const loggedUser = await Student.findById(user._id).select(
+      "-password -refresh_token"
+    );
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+    res
+      .status(200)
+      .json({
+        message: "User LoggedIn",
+        data: loggedUser,
+      })
+      .cookie("refreshtoken", refreshtoken, options)
+      .cookie("acesstoken", acesstoken, options);
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error In UserLogin",
+    });
+  }
+};
+
+export { userReg, userLogin };
