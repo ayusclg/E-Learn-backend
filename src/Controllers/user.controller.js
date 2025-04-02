9574fb963b804a633917c6a1605bb880832204eb
ayusclg <freelancerayus@gmail.com>
2025-04-02 12:56:52 +0545

    Updating User Details

--- src/Controllers/user.controller.js
+++ src/Controllers/user.controller.js
@@ -126,7 +126,7 @@ const userLogin = async (req, res) => {
   }
 };
 
-const studentFetch = async (req, res) => {
+const userFetch = async (req, res) => {
   try {
     const user = await Student.findById(req.Student._id).select(
       "-password -refresh_token"
@@ -174,4 +174,49 @@ const updatePassword = async (req, res) => {
     });
   }
 };
-export { userReg, userLogin, studentFetch, updatePassword };
+
+const updateUser = async (req, res) => {
+  try {
+    const { firstName, lastName, address, email, school, phone } = req.body;
+    
+    const user = await Student.findByIdAndUpdate(
+      req.Student._id,
+      {
+        $set: {
+          firstName,
+          lastName,
+          address,
+          email,
+          school,
+          phone,
+        },
+      },
+      {
+        new: true,
+      }
+    );
+    if (!user) {
+      return res.status(403).json({
+        message: "User Not Found",
+      });
+    }
+    const update = await Student.findById(user._id).select(
+      "-password -refresh_token"
+    );
+    if (!update) {
+      return res.status(400).json({
+        message: "User not Updated",
+      });
+    }
+
+    res.status(200).json({
+      message: "User Details Updated",
+      data: update,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error in Update User",
+    });
+  }
+};
+export { userReg, userLogin, userFetch, updatePassword, updateUser };
--- src/Routes/Student.routes.js
+++ src/Routes/Student.routes.js
@@ -1,7 +1,8 @@
 import Router from "express";
 import {
-  studentFetch,
   updatePassword,
+  updateUser,
+  userFetch,
   userLogin,
   userReg,
 } from "../Controllers/user.controller.js";
@@ -12,7 +13,8 @@ const router = Router();
 
 router.route("/sreg").post(Upload.single("studentPhoto"), userReg);
 router.route("/log").post(userLogin);
-router.route("/see").get(verifyToken, studentFetch);
+router.route("/see").get(verifyToken, userFetch);
 router.route("/upass").post(verifyToken, updatePassword);
+router.route("/uuser").post(verifyToken, updateUser);
 
 export default router;
