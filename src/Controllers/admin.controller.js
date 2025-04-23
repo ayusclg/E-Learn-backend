79efbbb9c9d64994773d78804deb4ccf45df6af9
ayusclg <freelancerayus@gmail.com>
2025-04-23 13:48:04 +0545

    User Delete

--- src/Controllers/admin.controller.js
+++ src/Controllers/admin.controller.js
@@ -39,4 +39,30 @@ const fetchUser = async (req, res) => {
   }
 };
 
-export { fetchUser };
+const deleteUser = async (req, res) => {
+  try {
+    const user = await Student.findById(req.Student._id).select(
+      "-password -refresh_token"
+    );
+    if (!user.isAdmin) {
+      return res.status(403).json({
+        message: "Unauthorized Access",
+      });
+    }
+    const deleteuser = await Student.findByIdAndDelete(req.params._id);
+    if (!deleteuser) {
+      return res.status(401).json({
+        messaged: "User Not Deleted",
+      });
+    }
+
+    res.status(200).json({
+      message: "User Successfully Deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error in deleting User",
+    });
+  }
+};
+export { fetchUser, deleteUser };
